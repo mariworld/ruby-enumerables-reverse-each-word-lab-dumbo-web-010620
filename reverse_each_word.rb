3c35c4b1ba782841f98338b66078ab2c622dce25
Demarius Edwards <demarius.edwards@gmail.com>
2019-12-14 06:34:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,12 +3,12 @@ def reverse_each_word(sentence)
   reversed = sentence.split
   revved = reversed.each(&:reverse!)
   joint = revved.join(" ")
-  binding.pry
+  #binding.pry
 end
 
-# def reverse_each_word(sentence)
-#   reversed = sentence.split
-#   revved = reversed.collect(&:reverse!)
-#   joint = revved.join(" ")
-#   binding.pry
-# end
+def reverse_each_word(sentence)
+  reversed = sentence.split
+  revved = reversed.collect(&:reverse!)
+  joint = revved.join(" ")
+  binding.pry
+end
