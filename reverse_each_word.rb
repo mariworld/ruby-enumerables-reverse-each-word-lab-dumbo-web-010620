249decbd1d3669cf7740e70943244b10233de620
Demarius Edwards <demarius.edwards@gmail.com>
2019-12-14 06:33:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,7 @@ def reverse_each_word(sentence)
   reversed = sentence.split
   revved = reversed.each(&:reverse!)
   joint = revved.join(" ")
+  binding.pry
 end
 
 # def reverse_each_word(sentence)
