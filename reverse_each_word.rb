98cb0de532ef25c80b668d2ab2533b67da16a43c
Demarius Edwards <demarius.edwards@gmail.com>
2019-12-14 06:26:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 require 'pry'
 def reverse_each_word(sentence)
   split_sentence = sentence.split
-  joint_sentence = split_sentence(&:reverse!)
+  split_sentence(&:reverse!)
   
     #binding.pry
 end
